694a79d4ac3a450f45b8dab8453d826952b40fc0
Al-Umairi <144678822+Al-Umairi@users.noreply.github.com>
2023-11-15 15:28:01 +0100

    Add files via upload

---
+++ sj23-24-wmc1-ORF_Elzanary/javascript/orf.js
@@ -0,0 +1,4 @@
+function toggleContactForm() {
+    var contactForm = document.getElementById("contact-form");
+    contactForm.style.display = (contactForm.style.display === "none" || contactForm.style.display === "") ? "block" : "none";
+}
